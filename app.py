f737d25bd1808b2e38c11a34f248dc697875d1a2
munibshah <97636532+munibshah@users.noreply.github.com>
2024-06-22 20:41:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def webhook():
     return jsonify({'message': 'Webhook received and processing started'}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT'))
     app.run(host='0.0.0.0', port=port)
